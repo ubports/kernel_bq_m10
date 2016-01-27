1ff16aca5747021dc78635426d99136fe944aefd
You-Sheng Yang <vicamo.yang@canonical.com>
2016-01-27 19:31:51 +0800

    re-generate boot.img on successful kernel build
    Bug: https://bugs.launchpad.net/avila-private/+bug/1538325

--- frieza-build.sh
+++ frieza-build.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+ABOOTIMG=${ABOOTIMG:-$(which abootimga || true)}
+
+if [ -z "${ABOOTIMG}" ]; then
+  echo "abootimg not found. Please install abootimg." >&2
+  exit 1
+fi
+
 KERNEL_DIR=$(dirname "$0")
 KERNEL_OUT=${KERNEL_OUT:-$(readlink -f "${KERNEL_DIR}/../KERNEL_OBJ")}
 KERNEL_ARCH=arm64
@@ -10,4 +17,6 @@ KERNEL_CROSS_COMPILE=${KERNEL_CROSS_COMPILE:-$(readlink -f "${KERNEL_DIR}/../cit
 
 mkdir -p "${KERNEL_OUT}"
 make -C "${KERNEL_DIR}" -j $(nproc) O="${KERNEL_OUT}" ARCH=${KERNEL_ARCH} CROSS_COMPILE="${KERNEL_CROSS_COMPILE}" ${KERNEL_DEFCONFIG} $@ \
-  && make -C "${KERNEL_DIR}" -j $(nproc) O="${KERNEL_OUT}" ARCH=${KERNEL_ARCH} CROSS_COMPILE="${KERNEL_CROSS_COMPILE}" $@
+  && make -C "${KERNEL_DIR}" -j $(nproc) O="${KERNEL_OUT}" ARCH=${KERNEL_ARCH} CROSS_COMPILE="${KERNEL_CROSS_COMPILE}" $@ \
+  && cp "${KERNEL_DIR}/boot.img" "${KERNEL_OUT}/boot.img" \
+  && abootimg -u "${KERNEL_OUT}/boot.img" -k "${KERNEL_OUT}/arch/arm64/boot/Image.gz-dtb"
