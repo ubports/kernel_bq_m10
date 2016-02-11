9606c834bdc72bebd7bd52837dc7e40e0c627550
Simon Fels <simon.fels@canonical.com>
2016-02-11 14:04:32 +0100

    drivers: mediatek: drop some more spamming debug print statements

--- drivers/misc/mediatek/spm/mt8163/mt_spm_internal.c
+++ drivers/misc/mediatek/spm/mt8163/mt_spm_internal.c
@@ -285,12 +285,6 @@ wake_reason_t __spm_output_wake_reason(const struct wake_status *wakesta,
 	}
 	BUG_ON(strlen(buf) >= LOG_BUF_SIZE);
 
-	spm_print(suspend, "wake up by%s, timer_out = %u, r13 = 0x%x, debug_flag = 0x%x\n",
-		  buf, wakesta->timer_out, wakesta->r13, wakesta->debug_flag);
-
-	spm_print(suspend, "r12 = 0x%x, raw_sta = 0x%x, idle_sta = 0x%x, event_reg = 0x%x, isr = 0x%x\n",
-		  wakesta->r12, wakesta->raw_sta, wakesta->idle_sta, wakesta->event_reg, wakesta->isr);
-
 	return wr;
 }
 
