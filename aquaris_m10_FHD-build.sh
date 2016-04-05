74086f8d545a9bc189b6026a1a6ac4cef9394569
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2016-04-05 17:31:17 +0200

    Change name of build script to public one

--- aquaris_m10_FHD-build.sh
+++ aquaris_m10_FHD-build.sh
@@ -12,7 +12,7 @@ fi
 KERNEL_DIR=$(dirname "$0")
 KERNEL_OUT=${KERNEL_OUT:-$(readlink -f "${KERNEL_DIR}/../KERNEL_OBJ")}
 KERNEL_ARCH=arm64
-KERNEL_DEFCONFIG=${KERNEL_DEFCONFIG:-frieza_defconfig}
+KERNEL_DEFCONFIG=${KERNEL_DEFCONFIG:-bq_aquaris_m10_FHD_ubuntu_defconfig}
 KERNEL_CROSS_COMPILE=${KERNEL_CROSS_COMPILE:-$(readlink -f "${KERNEL_DIR}/../cit-aarch64-linux-android-4.9")/bin/aarch64-linux-android-}
 
 mkdir -p "${KERNEL_OUT}"
