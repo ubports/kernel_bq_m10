547e2e991ba95c5b26870570eba4b32f934f0167
Marius Gripsgard <mariogrip@ubuntu.com>
2018-01-06 16:43:13 +0100

    Use the included toolchain

--- aquaris_m10_FHD-build.sh
+++ aquaris_m10_FHD-build.sh
@@ -13,9 +13,10 @@ KERNEL_DIR=$(dirname "$0")
 KERNEL_OUT=${KERNEL_OUT:-$(readlink -f "${KERNEL_DIR}/../KERNEL_OBJ")}
 KERNEL_ARCH=arm64
 KERNEL_DEFCONFIG=${KERNEL_DEFCONFIG:-bq_aquaris_m10_FHD_ubuntu_defconfig}
-KERNEL_CROSS_COMPILE=${KERNEL_CROSS_COMPILE:-$(readlink -f "${KERNEL_DIR}/../cit-aarch64-linux-android-4.9")/bin/aarch64-linux-android-}
+KERNEL_CROSS_COMPILE=${KERNEL_CROSS_COMPILE:-$(readlink -f "${KERNEL_DIR}/aarch64-linux-android-4.9")/bin/aarch64-linux-android-}
 
 mkdir -p "${KERNEL_OUT}"
+
 make -C "${KERNEL_DIR}" -j $(nproc) O="${KERNEL_OUT}" ARCH=${KERNEL_ARCH} CROSS_COMPILE="${KERNEL_CROSS_COMPILE}" ${KERNEL_DEFCONFIG} $@ \
   && make -C "${KERNEL_DIR}" -j $(nproc) O="${KERNEL_OUT}" ARCH=${KERNEL_ARCH} CROSS_COMPILE="${KERNEL_CROSS_COMPILE}" $@ \
   && cp "${KERNEL_DIR}/boot.img" "${KERNEL_OUT}/boot.img" \
